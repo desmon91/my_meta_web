bf128b02c291503b6b15c89b203260c62154a8fd
desmond kristian <desmondkristian@desmonds-MacBook-Air.local>
2022-07-25 12:38:42 +0800

    add icon to project list

--- components/ProjectModal.js
+++ components/ProjectModal.js
@@ -1,5 +1,4 @@
 import { useTransition, animated } from "react-spring";
-import Image from "next/image";
 export default function ProjectModal({
   hideModal,
   setHideModal,
@@ -69,135 +68,168 @@ export default function ProjectModal({
 
                 {/* text content */}
                 {/* list container */}
-                <div className="my-2 text-left">
-                  <h3 className="text-3xl font-semibold text-orange-500">
-                    Latest Project
-                  </h3>
-                </div>
-                {/* item container */}
+                <div class="w-full h-full p-5">
+                  <div className="my-2 text-left">
+                    <h3 className="text-3xl font-semibold text-orange-500">
+                      Latest Project
+                    </h3>
+                  </div>
+                  {/* item container */}
 
-                <div className="flex justify-between items-center flex-col sm:flex-row">
-                  <div className="w-full">
-                    <div className="my-5 text-left max-w-xs">
-                      <h3 className="text-xl font-semibold text-gray-700">
-                        Motix App
-                      </h3>
-                      <p className="text-sm text-gray-500">
-                        Design a mobile ticketing app for a movie theater
-                      </p>
+                  <div className="flex justify-between items-center flex-col">
+                    <div className="w-full flex flex-row flex-nowrap items-center">
+                      <img
+                        alt=""
+                        src="/design.png"
+                        className="w-10 h-10 mr-5"
+                      />
+                      <div className="my-5 text-left">
+                        <h3 className="text-xl font-semibold text-gray-700">
+                          Motix App
+                        </h3>
+                        <p className="text-sm text-gray-500">
+                          Design a mobile ticketing app for a movie theater
+                        </p>
+                      </div>
+                    </div>
+                    <div class="flex w-full justify-end">
+                      <button
+                        className="text-orange-500 p-2 hover:text-gray-500 hover:border-gray-500 border-2 rounded-lg border-orange-500"
+                        onClick={() => openInNewTab("/project/motix")}
+                      >
+                        Case Study
+                      </button>
                     </div>
                   </div>
-                  <div>
-                    <button
-                      className="text-orange-500 p-2 hover:text-gray-500 hover:border-gray-500 border-2 rounded-lg border-orange-500"
-                      onClick={() => openInNewTab("/project/motix")}
-                    >
-                      Case Study
-                    </button>
-                  </div>
-                </div>
-                <div className="flex justify-between items-center flex-col sm:flex-row">
-                  <div className="w-full">
-                    <div className="my-5 text-left max-w-xs">
-                      <h3 className="text-xl font-semibold text-gray-700">
-                        DIY Hub
-                      </h3>
-                      <p className="text-sm text-gray-500">
-                        Design responsive website for DIY tutorials
-                      </p>
+                  <div className="flex justify-between items-center flex-col">
+                    <div className="w-full flex flex-row flex-nowrap items-center">
+                      <img
+                        alt=""
+                        src="/design.png"
+                        className="w-10 h-10 mr-5"
+                      />
+                      <div className="my-5 text-left">
+                        <h3 className="text-xl font-semibold text-gray-700">
+                          DIY Hub
+                        </h3>
+                        <p className="text-sm text-gray-500">
+                          Design responsive website for DIY tutorials
+                        </p>
+                      </div>
+                    </div>
+                    <div class="flex w-full justify-end">
+                      <button
+                        className="text-orange-500 p-2 hover:text-gray-500 hover:border-gray-500 border-2 rounded-lg border-orange-500"
+                        onClick={() => openInNewTab("/project/diy")}
+                      >
+                        Case Study
+                      </button>
                     </div>
                   </div>
-                  <div>
-                    <button
-                      className="text-orange-500 p-2 hover:text-gray-500 hover:border-gray-500 border-2 rounded-lg border-orange-500"
-                      onClick={() => openInNewTab("/project/diy")}
-                    >
-                      Case Study
-                    </button>
-                  </div>
-                </div>
-                <div className="flex justify-between items-center flex-col sm:flex-row">
-                  <div className="w-full">
-                    <div className="my-5 text-left max-w-xs">
-                      <h3 className="text-xl font-semibold text-gray-700">
-                        Talen App
-                      </h3>
-                      <p className="text-sm text-gray-500">
-                        Design a cross-platform tool to help people learn how to
-                        read in a new language
-                      </p>
+                  <div className="flex justify-between items-center flex-col">
+                    <div className="w-full flex flex-row flex-nowrap items-center">
+                      <img
+                        alt=""
+                        src="/design.png"
+                        className="w-10 h-10 mr-5"
+                      />
+                      <div className="my-5 text-left">
+                        <h3 className="text-xl font-semibold text-gray-700">
+                          Talen App
+                        </h3>
+                        <p className="text-sm text-gray-500">
+                          Design a cross-platform tool to help people learn how
+                          to read in a new language
+                        </p>
+                      </div>
+                    </div>
+                    <div class="flex w-full justify-end">
+                      <button
+                        className="text-orange-500 p-2 hover:text-gray-500 hover:border-gray-500 border-2 rounded-lg border-orange-500"
+                        onClick={() => openInNewTab("/project/talen")}
+                      >
+                        Case Study
+                      </button>
                     </div>
                   </div>
-                  <div>
-                    <button
-                      className="text-orange-500 p-2 hover:text-gray-500 hover:border-gray-500 border-2 rounded-lg border-orange-500"
-                      onClick={() => openInNewTab("/project/talen")}
-                    >
-                      Case Study
-                    </button>
-                  </div>
-                </div>
-                <div className="flex justify-between items-center flex-col sm:flex-row">
-                  <div className="w-full">
-                    <div className="my-5 text-left max-w-xs">
-                      <h3 className="text-xl font-semibold text-gray-700">
-                        HDM App
-                      </h3>
-                      <p className="text-sm text-gray-500">
-                        Create an end-to-end internal web application for the
-                        company Health Database Management (HDM) clinic
-                      </p>
+                  <div className="flex justify-between items-center flex-col">
+                    <div className="w-full flex flex-row flex-nowrap items-center">
+                      <img
+                        alt=""
+                        src="/web-programming.png"
+                        className="w-10 h-10 mr-5"
+                      />
+                      <div className="my-5 text-left">
+                        <h3 className="text-xl font-semibold text-gray-700">
+                          HDM App
+                        </h3>
+                        <p className="text-sm text-gray-500">
+                          Create an end-to-end internal web application for the
+                          company Health Database Management (HDM) clinic
+                        </p>
+                      </div>
+                    </div>
+                    <div class="flex w-full justify-end">
+                      <button
+                        className="text-orange-500 p-2 hover:text-gray-500 hover:border-gray-500 border-2 rounded-lg border-orange-500"
+                        onClick={() => openInNewTab("/project/hdm")}
+                      >
+                        Case Study
+                      </button>
                     </div>
                   </div>
-                  <div>
-                    <button
-                      className="text-orange-500 p-2 hover:text-gray-500 hover:border-gray-500 border-2 rounded-lg border-orange-500"
-                      onClick={() => openInNewTab("/project/hdm")}
-                    >
-                      Case Study
-                    </button>
-                  </div>
-                </div>
-                <div className="flex justify-between items-center flex-col sm:flex-row">
-                  <div className="w-full">
-                    <div className="my-5 text-left max-w-xs">
-                      <h3 className="text-xl font-semibold text-gray-700">
-                        eSimper App
-                      </h3>
-                      <p className="text-sm text-gray-500">
-                        Create an end-to-end internal web application for
-                        company driving license administration process
-                      </p>
+
+                  <div className="flex justify-between items-center flex-col">
+                    <div className="w-full flex flex-row flex-nowrap items-center">
+                      <img
+                        alt=""
+                        src="/web-programming.png"
+                        className="w-10 h-10 mr-5"
+                      />
+                      <div className="my-5 text-left">
+                        <h3 className="text-xl font-semibold text-gray-700">
+                          eSimper App
+                        </h3>
+                        <p className="text-sm text-gray-500">
+                          Create an end-to-end internal web application for
+                          company driving license administration process
+                        </p>
+                      </div>
+                    </div>
+                    <div class="flex w-full justify-end">
+                      <button
+                        className="text-orange-500 p-2 hover:text-gray-500 hover:border-gray-500 border-2 rounded-lg border-orange-500"
+                        onClick={() => openInNewTab("/project/esimper")}
+                      >
+                        Case Study
+                      </button>
                     </div>
                   </div>
-                  <div>
-                    <button
-                      className="text-orange-500 p-2 hover:text-gray-500 hover:border-gray-500 border-2 rounded-lg border-orange-500"
-                      onClick={() => openInNewTab("/project/esimper")}
-                    >
-                      Case Study
-                    </button>
-                  </div>
-                </div>
-                <div className="flex justify-between items-center flex-col sm:flex-row">
-                  <div className="w-full">
-                    <div className="my-5 text-left max-w-xs">
-                      <h3 className="text-xl font-semibold text-gray-700">
-                        iRemember App
-                      </h3>
-                      <p className="text-sm text-gray-500">
-                        Create an end-to-end internal web application for
-                        compliance document tracking and monitoring
-                      </p>
+                  <div className="flex justify-between items-center flex-col">
+                    <div className="w-full flex flex-row flex-nowrap items-center">
+                      <img
+                        alt=""
+                        src="/web-programming.png"
+                        className="w-10 h-10 mr-5"
+                      />
+                      <div className="my-5 text-left">
+                        <h3 className="text-xl font-semibold text-gray-700">
+                          iRemember App
+                        </h3>
+                        <p className="text-sm text-gray-500">
+                          Create an end-to-end internal web application for
+                          compliance document tracking and monitoring
+                        </p>
+                      </div>
                     </div>
-                  </div>
-                  <div>
-                    <button
-                      className="text-orange-500 p-2 hover:text-gray-500 hover:border-gray-500 border-2 rounded-lg border-orange-500"
-                      onClick={() => openInNewTab("/project/iremember")}
-                    >
-                      Case Study
-                    </button>
+                    <div class="flex w-full justify-end">
+                      <button
+                        className="text-orange-500 p-2 hover:text-gray-500 hover:border-gray-500 border-2 rounded-lg border-orange-500"
+                        onClick={() => openInNewTab("/project/iremember")}
+                      >
+                        Case Study
+                      </button>
+                    </div>
                   </div>
                 </div>
               </div>

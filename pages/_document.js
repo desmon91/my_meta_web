16529c41e5a1f520d6fd134884fd9f9fbc4c0bac
desmond kristian <desmondkristian@desmonds-MacBook-Air.local>
2022-07-25 13:04:01 +0800

    add custom html meta tags

---
+++ pages/_document.js
@@ -0,0 +1,20 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta name="description" content="Desmond Kristian Website Portfolio" />
+        <meta
+          name="keywords"
+          content="Full Stack Developer, UX Designer, Desmond Kristian"
+        />
+        <meta name="author" content="Desmond Kristian" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}

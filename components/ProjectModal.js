eb2209e82237dec938f17162d8c9045c048b9c2c
desmond kristian <desmondkristian@desmonds-MacBook-Air.local>
2022-07-25 13:06:26 +0800

    fix class to className

--- components/ProjectModal.js
+++ components/ProjectModal.js
@@ -68,7 +68,7 @@ export default function ProjectModal({
 
                 {/* text content */}
                 {/* list container */}
-                <div class="w-full h-full p-5">
+                <div className="w-full h-full p-5">
                   <div className="my-2 text-left">
                     <h3 className="text-3xl font-semibold text-orange-500">
                       Latest Project
@@ -92,7 +92,7 @@ export default function ProjectModal({
                         </p>
                       </div>
                     </div>
-                    <div class="flex w-full justify-end">
+                    <div className="flex w-full justify-end">
                       <button
                         className="text-orange-500 p-2 hover:text-gray-500 hover:border-gray-500 border-2 rounded-lg border-orange-500"
                         onClick={() => openInNewTab("/project/motix")}
@@ -117,7 +117,7 @@ export default function ProjectModal({
                         </p>
                       </div>
                     </div>
-                    <div class="flex w-full justify-end">
+                    <div className="flex w-full justify-end">
                       <button
                         className="text-orange-500 p-2 hover:text-gray-500 hover:border-gray-500 border-2 rounded-lg border-orange-500"
                         onClick={() => openInNewTab("/project/diy")}
@@ -143,7 +143,7 @@ export default function ProjectModal({
                         </p>
                       </div>
                     </div>
-                    <div class="flex w-full justify-end">
+                    <div className="flex w-full justify-end">
                       <button
                         className="text-orange-500 p-2 hover:text-gray-500 hover:border-gray-500 border-2 rounded-lg border-orange-500"
                         onClick={() => openInNewTab("/project/talen")}
@@ -169,7 +169,7 @@ export default function ProjectModal({
                         </p>
                       </div>
                     </div>
-                    <div class="flex w-full justify-end">
+                    <div className="flex w-full justify-end">
                       <button
                         className="text-orange-500 p-2 hover:text-gray-500 hover:border-gray-500 border-2 rounded-lg border-orange-500"
                         onClick={() => openInNewTab("/project/hdm")}
@@ -196,7 +196,7 @@ export default function ProjectModal({
                         </p>
                       </div>
                     </div>
-                    <div class="flex w-full justify-end">
+                    <div className="flex w-full justify-end">
                       <button
                         className="text-orange-500 p-2 hover:text-gray-500 hover:border-gray-500 border-2 rounded-lg border-orange-500"
                         onClick={() => openInNewTab("/project/esimper")}
@@ -222,7 +222,7 @@ export default function ProjectModal({
                         </p>
                       </div>
                     </div>
-                    <div class="flex w-full justify-end">
+                    <div className="flex w-full justify-end">
                       <button
                         className="text-orange-500 p-2 hover:text-gray-500 hover:border-gray-500 border-2 rounded-lg border-orange-500"
                         onClick={() => openInNewTab("/project/iremember")}

9395f756cdafa75a79bb91c5991e5326a78e67d0
desmond kristian <desmondkristian@desmonds-MacBook-Air.local>
2022-07-27 07:33:02 +0800

    add google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,23 @@ export default function Document() {
           content="Full Stack Developer, UX Designer, Desmond Kristian"
         />
         <meta name="author" content="Desmond Kristian" />
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
       </Head>
       <body>
         <Main />

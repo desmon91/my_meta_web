8005ac37233c10cb5b4907b2c1de00c54fc87a94
desmond kristian <desmondkristian@desmonds-MacBook-Air.local>
2022-08-04 10:47:03 +0800

    add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: process.env.NEXT_URL || "https://desmond-kristian.my.id",
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+};
+
+export default config;
